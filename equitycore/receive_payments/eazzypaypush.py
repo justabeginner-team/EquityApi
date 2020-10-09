670adc5d2651fcce10669b93653e4820422f1054
IanMaguithi <maguithian13427@gmail.com>
2020-10-09 15:37:23 +0300

    bill and merchant payments added

--- equitycore/receive_payments/eazzypaypush.py
+++ equitycore/receive_payments/eazzypaypush.py
@@ -29,15 +29,17 @@ def eazzypay_push(
     trans_type = 'EazzyPayOnline'
 
     trans_data = (str(trans_ref), str(trans_amount), merchant_code, countryCode)
-    signed_signature = signature(trans_data)
+    signed_data = signature(trans_data)
+
     payload = "{{\r\n   \"customer\": {{\r\n      \"mobileNumber\": \"{0}\",\r\n   \"countryCode\": \"{1}\"\r\n  }}," \
               "\r\n   \"transaction\": {{\r\n      \"amount\": \"{2}\",\r\n      \"description\": \"{3}\"," \
               "\r\n    \"type\": \"{4}\",\r\n      \"reference\": \"{5}\"\r\n   }}\r\n}}".format(
         str(mssid), countryCode, str(trans_amount), trans_desc, trans_type, trans_ref)
+
     headers = {
         'Authorization': token,
         'Content-Type': 'application/json',
-        'signature': signed_signature
+        'signature': signed_data
     }
 
     url = f"{settings.UAT_URL}/transaction/v2/payments"
