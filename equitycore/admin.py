23bf8f3a1d7671cd7e7a63832d16b437dc117067
IanMaguithi <maguithian13427@gmail.com>
2020-10-18 17:05:10 +0300

    registered added models to admin.py

--- equitycore/admin.py
+++ equitycore/admin.py
@@ -12,12 +12,12 @@ admin.site.register(AuthToken, AuthTokenAdmin)
 
 @admin.register(EazzyPushRequest)
 class EazzyPushRequestAdmin(admin.ModelAdmin):
-    list_display = ( 
-                    "customer_phone_number",
-                    "transaction_amount",  
-                    "date_added", 
-                    "transaction_date"
-                    )
+    list_display = (
+        "customer_phone_number",
+        "transaction_amount",
+        "date_added",
+        "transaction_date"
+    )
     readonly_fields = (
         "customer_phone_number",
         "transaction_amount",
@@ -32,13 +32,13 @@ class EazzyPushRequestAdmin(admin.ModelAdmin):
         "is_posted",
         "paid",
     )
-    search_fields = ( 
-                     "customer_phone_number",
-                     "transaction_amount", 
-                     "date_added", 
-                     "transaction_date", 
-                     "customer_country_code"
-                     )
+    search_fields = (
+        "customer_phone_number",
+        "transaction_amount",
+        "date_added",
+        "transaction_date",
+        "customer_country_code"
+    )
     list_filter = (("date_added", DateRangeFilter),
                    ("transaction_date", DateRangeFilter),)
 
@@ -46,11 +46,11 @@ class EazzyPushRequestAdmin(admin.ModelAdmin):
 @admin.register(LipanampesaRequest)
 class LipaNaMpesaRequestAdmin(admin.ModelAdmin):
     list_display = (
-                    "customer_phone_number",
-                    "transaction_amount", 
-                    "date_added", 
-                    "transaction_date"
-                    )
+        "customer_phone_number",
+        "transaction_amount",
+        "date_added",
+        "transaction_date"
+    )
     readonly_fields = (
         "customer_phone_number",
         "transaction_amount",
@@ -61,31 +61,31 @@ class LipaNaMpesaRequestAdmin(admin.ModelAdmin):
         "transaction_date",
         "date_added",
         "transaction_status",
-       
+
         "paid",
     )
     search_fields = (
-                     "customer_phone_number",
-                     "transaction_amount", 
-                     "date_added", 
-                     "transaction_date", 
-                     "customer_country_code"
-                     )
+        "customer_phone_number",
+        "transaction_amount",
+        "date_added",
+        "transaction_date",
+        "customer_country_code"
+    )
     list_filter = (
-                    ("date_added", DateRangeFilter),
-                    ("transaction_date", DateRangeFilter),
-                  )
+        ("date_added", DateRangeFilter),
+        ("transaction_date", DateRangeFilter),
+    )
 
 
 @admin.register(MerchantRequest)
 class MerchantRequestAdmin(admin.ModelAdmin):
     list_display = (
-                    "partner_id",
-                    "partner_reference",
-                    "transaction_amount", 
-                    "date_added", 
-                    "transaction_date"
-                    )
+        "partner_id",
+        "partner_reference",
+        "transaction_amount",
+        "date_added",
+        "transaction_date"
+    )
     readonly_fields = (
         "partner_id",
         "merchant_till",
@@ -101,14 +101,87 @@ class MerchantRequestAdmin(admin.ModelAdmin):
         "paid",
     )
     search_fields = (
-                    "partner_reference",
-                    "partner_id",
-                     "transaction_amount", 
-                     "date_added", 
-                     "transaction_date", 
-                     "currency"
-                     )
+        "partner_reference",
+        "partner_id",
+        "transaction_amount",
+        "date_added",
+        "transaction_date",
+        "currency"
+    )
     list_filter = (
-                   ("date_added", DateRangeFilter),
-                   ("transaction_date", DateRangeFilter),
-                   )
+        ("date_added", DateRangeFilter),
+        ("transaction_date", DateRangeFilter),
+    )
+
+
+@admin.register(BillPaymentRequest)
+class BillPaymentRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "partner_id",
+        'biller_code',
+        "bill_reference",
+        "bill_amount",
+        "date_added",
+        "transaction_date"
+    )
+    readonly_fields = (
+        "partner_id",
+        "biller_code",
+        "bill_amount",
+        "bill_currency",
+        "payer_reference",
+        "payer_mobile_number",
+        "api_transaction_reference",
+        "remarks",
+        "transaction_date",
+        "date_added",
+        "transaction_status",
+        "is_posted",
+        "paid",
+    )
+    search_fields = (
+        "payer_reference",
+        "partner_id",
+        "bill_amount",
+        "date_added",
+        "transaction_date",
+        "bill_currency"
+    )
+    list_filter = (
+        ("date_added", DateRangeFilter),
+        ("transaction_date", DateRangeFilter),
+    )
+
+
+@admin.register(BillValidationRequest)
+class BillValidationRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "biller_code",
+        'customer_reference_number',
+        "bill_currency",
+        "bill_amount",
+        "date_added",
+        "transaction_date"
+    )
+    readonly_fields = (
+        "biller_code",
+        'customer_reference_number',
+        "bill_currency",
+        "bill_amount",
+        "date_added",
+        "transaction_date",
+        "date_added",
+        "is_posted",
+        "validated",
+    )
+    search_fields = (
+        "customer_reference_number",
+        'biller_code',
+        "date_added",
+        "transaction_date",
+        "bill_currency"
+    )
+    list_filter = (
+        ("date_added", DateRangeFilter),
+        ("transaction_date", DateRangeFilter),
+    )
